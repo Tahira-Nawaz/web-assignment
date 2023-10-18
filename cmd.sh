b2b2c58d1eb881a2cb27d221840b1eb146ff0315
Tahira-Nawaz <tnawaz@puffersoft.com>
2023-10-18 14:44:59 +0500

    commit by tahira

--- cmd.sh
+++ cmd.sh
@@ -1,4 +1,5 @@
-LOCATION='eastus2'
-RESOURCEGROUPNAME='tahira10-rg'
-az group create --name $RESOURCEGROUPNAME --location $LOCATION
-az deployment group create --resource-group $RESOURCEGROUPNAME --template-file web.bicep --parameters web.parameters.json
+# LOCATION='eastus2'
+# RESOURCEGROUPNAME='tahira10-rg'
+# az group create --name $RESOURCEGROUPNAME --location $LOCATION
+# az deployment group create --resource-group $RESOURCEGROUPNAME --template-file web.bicep --parameters web.parameters.json
+az deployment sub create --name Microsoft-Partner-Network --location eastus --template-file ./main.bicep --parameters ./main.parameters.json
